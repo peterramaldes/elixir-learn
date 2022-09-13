7f0063690a15e1ee8da81e0f8480c76b12765cf2
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2022-09-13 18:13:53 -0300

    Live View with Big Integer (milliseconds). -- I guess :)

--- big_integer_live_view/lib/big_integer/big_integer_display.ex
+++ big_integer_live_view/lib/big_integer/big_integer_display.ex
@@ -1,9 +1,23 @@
 defmodule BigIntegerLiveViewWeb.Live.BigIntegerDisplay do
   use BigIntegerLiveViewWeb, :live_view
 
+  def mount(_params, _session, socket) do
+    {:ok, schedule_tick_and_update_assign(socket)}
+  end
+
+  def handle_info(:tick, socket) do
+    {:noreply, schedule_tick_and_update_assign(socket)}
+  end
+
+  defp schedule_tick_and_update_assign(socket) do
+    Process.send_after(self(), :tick, 1000)
+    assign(socket, :now, :os.system_time(:millisecond))
+  end
+
   def render(assigns) do
     ~L"""
-      <h1> Hi from Big Integer Live View </h1>
+      <h3>Now:</h3>
+      <p><%= assigns.now %></p>
     """
   end
 end
