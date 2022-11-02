fb43ad05bedf9e816d2f66168c5d73d41bba9830
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2022-11-02 17:17:17 -0300

    Added POST for businesses!

--- busi_api/priv/scripts/post/bussiness.sh
+++ busi_api/priv/scripts/post/bussiness.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-curl -X POST http://localhost:4000/api/businesses \
--H “Content-Type: application/json” \
--d ‘{“business”: {“name”: “Yet another company”, “description”: “Another short description!”, “tag”: “IT”}}’
+curl -sv http://localhost:4000/api/businesses \
+-H "Content-Type: application/json" \
+-d '{"business": {"name": "Yet another company", "description": "Another short description!", "tag": "IT"}}' \
+| jq
