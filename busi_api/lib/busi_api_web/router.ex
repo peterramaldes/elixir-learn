aa7e137e63f1356cf4bd43e9ad0f15b65d9c8542
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2022-11-02 15:01:46 -0300

    Created first route

--- busi_api/lib/busi_api_web/router.ex
+++ busi_api/lib/busi_api_web/router.ex
@@ -9,6 +9,15 @@ defmodule BusiApiWeb.Router do
     pipe_through :api
   end
 
+  pipeline :browser do
+    plug(:accepts, ["html"])
+  end
+
+  scope "/", BusiApiWeb do
+    pipe_through :browser
+    get "/", DefaultController, :index
+  end
+
   # Enables LiveDashboard only for development
   #
   # If you want to use the LiveDashboard in production, you should put
