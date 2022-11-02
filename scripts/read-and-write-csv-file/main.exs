cffe73b509462e805d540c04f137c019e1f42f7b
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2022-11-02 13:46:52 -0300

    Find specific value in array of struct

--- scripts/read-and-write-csv-file/main.exs
+++ scripts/read-and-write-csv-file/main.exs
@@ -2,29 +2,35 @@ defmodule Oscar do
   defstruct [:index, :year, :age, :name, :movie]
 
   def get_oscar_from_csv(csv_filepath) do
-    arr =
+    x =
       File.read!(csv_filepath)
       |> String.split("\n")
       |> Enum.map(&String.split(&1, ",", trim: true))
-      |> IO.inspect()
 
-    [_ | t] =
-      arr
-      |> Enum.map(fn [a, b, c, d, e] ->
-        %Oscar{
-          index: String.trim(a),
-          year: String.trim(b),
-          age: String.trim(c),
-          name: String.replace(String.trim(d), "\"", ""),
-          movie: String.replace(String.trim(e), "\"", "")
-        }
-      end)
+    [_ | tail] = x
+
+    tail
+    |> Enum.map(fn [a, b, c, d, e] ->
+      %Oscar{
+        index: String.trim(a),
+        year: String.trim(b),
+        age: String.trim(c),
+        name: String.replace(String.trim(d), "\"", ""),
+        movie: String.replace(String.trim(e), "\"", "")
+      }
+    end)
   end
 end
 
 defmodule Main do
   def main do
+    oscar_name =
+      IO.gets("What is the name?\n")
+      |> String.trim()
+      |> String.downcase()
+
     Oscar.get_oscar_from_csv("assets/oscar_age_male.csv")
+    |> Enum.find(fn x -> oscar_name == String.downcase(x.name) end)
     |> IO.inspect()
   end
 end
